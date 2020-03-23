c85e5a546c92e43db2d8dfd2acb527cadf3bf95e
XIAOLONGLII <1491skdpf89@gmail.com>
2020-03-23 17:08:54 -0500

    Updated migration file

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -18,7 +18,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superUser(self, email, name, password):
+    def create_superuser(self, email, name, password):
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
@@ -31,7 +31,7 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
